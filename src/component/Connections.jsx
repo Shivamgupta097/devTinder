fa9e61ec26bd713cb437de3d1375639887b1657d
shivam <shivam.applicant@gmail.com>
2026-02-17 17:55:30 +0530

    Integerated request api

--- src/component/Connections.jsx
+++ src/component/Connections.jsx
@@ -1,17 +1,43 @@
+import { useEffect, useState } from "react";
+import { BASE_URL } from "../utils/constants";
+import axios from "axios";
+
 const Connections = () => {
+  const [connections, setConnections] = useState([]);
 
-  const [connections , setConnection] = useState([])
+  async function getUserConnection() {
+    try {
+      const res = await axios.get(BASE_URL + "/user/connection", {
+        withCredentials: true,
+      });
 
- async function getUserConnection(){
-  const data = await axios.get()
+      setConnections(res?.data?.data);
+    } catch (error) {
+      console.log(error, "error");
+    }
   }
 
-  useEffect(() =>{
-    getUserConnection()
-  },[])
+  useEffect(() => {
+    getUserConnection();
+  }, []);
   return (
-    <div>Connections</div>
-  )
-}
+    <div className="flex gap-3 m-3">
+    {connections.map((c) => <div
+      className="card bg-base-100 w-65 shadow-sm bg-green place-items-center "
+      style={{ border: "1px solid white", maxHeight: "500px"  }}
+    >
+    
+      <div>
+        <img src={c?.toUserId?.photoUrl} alt="Shoes" />
+      </div>
+      <div className="card-body">
+        <h2 className="card-title">{c?.toUserId?.firstName + " " + c?.toUserId?.lastName}</h2>
+        <p>{c?.toUserId?.age && c?.toUserId?.gender && c?.toUserId?.age + "," + c?.toUserId?.gender}</p>
+        <p>{c?.toUserId?.about}</p>
+      </div>
+    </div>)}
+    </div>
+  );
+};
 
-export default Connections
+export default Connections;
--- src/component/Requests.jsx
+++ src/component/Requests.jsx
@@ -1,7 +1,92 @@
-const Requests = () => {
-  return (
-    <div>Requests</div>
-  )
-}
+import { useEffect, useState } from "react";
+import { BASE_URL } from "../utils/constants";
+import axios from "axios";
 
-export default Requests
+const Requests = () => {
+  const [requests, setRequests] = useState([]);
+  const [showButton, setShowButton] = useState(true)
+
+  async function getUserRequests() {
+    try {
+      const res = await axios.get(BASE_URL + "/user/request/recieved", {
+        withCredentials: true,
+      });
+
+      setRequests(res?.data?.data);
+    } catch (error) {
+      console.log(error, "error");
+    }
+  }
+
+  async function toAcceptAndRejectUser(currentStatus, requestedId) {
+    try {
+      const res = await axios.post(
+        BASE_URL + `/request/review/${currentStatus}/${requestedId}`,
+        {},
+        {
+          withCredentials: true,
+        },
+      );
+
+      setRequests(res?.data?.data);
+    } catch (error) {
+      console.log(error, "error");
+    }
+  }
+
+  function handleUser(currentStatus, requestedId) {
+    // setStatus(status);
+    // setRequestedId(requestedId)
+    toAcceptAndRejectUser(currentStatus, requestedId);
+  }
+  useEffect(() => {
+    getUserRequests();
+  }, []);
+
+  if(!requests || !requests.length){
+    return <h1>Hello</h1>
+  }
+
+  return (
+    <div className="flex gap-3 m-3">
+      {requests.map((c) => (
+        <div
+          className="card bg-base-100 w-65 shadow-sm bg-green place-items-center my-3 "
+          style={{ border: "1px solid white", maxHeight: "500px" }}
+          key={c?._id}
+        >
+          <div>
+            <img src={c?.fromUserId?.photoUrl} alt="Shoes" />
+          </div>
+          <div className="card-body">
+            <h2 className="card-title">
+              {c?.fromUserId?.firstName + " " + c?.fromUserId?.lastName}
+            </h2>
+            <p>
+              {c?.fromUserId?.age &&
+                c?.fromUserId?.gender &&
+                c?.fromUserId?.age + "," + c?.fromUserId?.gender}
+            </p>
+            <p>{c?.fromUserId?.about}</p>
+          </div>
+          {(showButton !== "Accepted" || showButton !=='rejected')  && <div>
+            <button
+              className="btn btn-primary me-3"
+              onClick={() => handleUser("accepted", c?._id)}
+            >
+              Accepted
+            </button>
+            <button
+              className="btn btn-secondary "
+              onClick={() => handleUser("rejected", c?._id)}
+            >
+              Rejected
+            </button>
+          </div>}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Requests;
